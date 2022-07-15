abbfd35396d39f6d4c2b4297c461590b544549ee
nicknufayl <nikndaniel@gmail.com>
2022-07-15 17:52:13 +0800

    Refactor app.js
    Refactored a bit of the JavaScript

--- app.js
+++ app.js
@@ -3,20 +3,14 @@ let compScore = 0
 
 function computerPlay() {
     const arrOfChoices = ['rock', 'paper', 'scissors']
-    const randomNum = Math.floor(Math.random() * 3)
-    const compChoice = arrOfChoices[randomNum]
-    return compChoice
+    const randomNum = Math.floor(Math.random() * arrOfChoices.length)
+    return compChoice = arrOfChoices[randomNum]
 }
 
 function playRound(playerSelection, computerSelection) {
     //
-    if (playerSelection === 'rock' && computerSelection === 'rock') {
-        return 'You tied! You both picked rock'
-    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'You tied! You both picked paper'
-    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        return 'You tied! You both picked scissors'
-    //
+    if (playerSelection === computerSelection) {
+        return `You tied! You both picked ${playerSelection}` 
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         compScore++
         return 'You LOST! Paper covers rock'
