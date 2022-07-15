6d58a56c18f9fb090845ecebc8987bd1dfd44850
nicknufayl <nikndaniel@gmail.com>
2022-07-15 17:38:12 +0800

    Finish basic requirements
    Done with the project with the minimum requirements

--- app.js
+++ app.js
@@ -18,33 +18,42 @@ function playRound(playerSelection, computerSelection) {
         return 'You tied! You both picked scissors'
     //
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
         return 'You LOST! Paper covers rock'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
         return 'You WON! Rock crushes scissors'
     //
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
         return 'You LOST! Scissors cuts paper'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
         return 'You WON! Paper covers rock'
     //
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
         return 'You LOST! Rock crushes scissors'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
         return 'You WON! Scissors cuts paper'
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-}
-
-const playerSelection = "rock"
-
 function game() {
     for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Rock, Paper or Scissors?', '').toLowerCase()
         const computerSelection = computerPlay()
         console.log(playRound(playerSelection, computerSelection))
     }
+
+    if (playerScore > compScore) {
+        return 'VICTORY!'
+    } else if (playerScore < compScore) {
+        return 'DEFEATED!'
+    } else {
+        return 'TIED? HOW?'
+    }
 }
 
-game()
+console.log(game())
